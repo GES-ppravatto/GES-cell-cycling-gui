8b7090db130c7bca9e9c84a88e10254eb8fb8295
GES-ppravatto <pierpaolo.pravatto@greenenergystorage.eu>
2022-07-21 16:17:57 +0200

    Added comments to the code to improve readability

--- cell-cycling/data_upload.py
+++ cell-cycling/data_upload.py
@@ -1,4 +1,3 @@
-import sys
 import streamlit as st
 import pandas as pd
 from io import BytesIO
@@ -7,6 +6,10 @@ from echemsuite.cellcycling.file_manager import FileManager, Instrument
 
 
 def set_default() -> None:
+    """
+    Set the default values for the streamlit session state to start the
+    application in a fresh state
+    """
     st.session_state["num_uploaded_files"] = 0
     st.session_state["uploader_expanded"] = True
     st.session_state["file_manager"] = None
@@ -20,7 +23,7 @@ def set_default() -> None:
     st.session_state["cellcycles"] = None
 
 
-# inital setup of the session state variables
+# On startup set the session state with the default values
 if "uploader_expanded" not in st.session_state:
     set_default()
 
@@ -28,10 +31,9 @@ st.set_page_config(layout="wide")
 
 st.title("Cell-cycling")
 
-
-with st.expander(
-    "Files upload section", expanded=st.session_state["uploader_expanded"]
-):
+# Define the expander containing the GUI for uploading new files
+with st.expander("Files upload section", expanded=st.session_state["uploader_expanded"]):
+    # Define an upload form to load the datafiles to be analyzed
     with st.form("Upload form", clear_on_submit=True):
         files = st.file_uploader(
             "Select the cell-cycling datafiles", accept_multiple_files=True
@@ -46,6 +48,7 @@ with st.expander(
                 )  # Create custom exceptions
 
             else:
+                # Generate a BytesIO stream buffer and check if the encoding is a valid utf-8
                 bytestreams = {}
                 for file in files:
                     stream = BytesIO(file.getvalue())
@@ -57,6 +60,7 @@ with st.expander(
                         stream.seek(0)
                         bytestreams[file.name] = stream
 
+                # Generate an instance of the FileManager class and load the bytestream buffer
                 manager = FileManager(verbose=False)
 
                 if extensions[0].lower() == ".dta":
@@ -68,25 +72,26 @@ with st.expander(
                 manager.bytestreams = bytestreams
                 manager.parse()
 
-                suggested_ordering = manager.suggest_ordering()
-                st.write(suggested_ordering)
-
                 st.session_state["number_of_halfcycles_files"] = len(manager.halfcycles)
                 st.session_state["file_manager"] = manager
 
+            # Set the fold option for the current expander and make the page rerun to make the change effective
             st.session_state["uploader_expanded"] = False
             st.experimental_rerun()
 
 
+# Prepare the GUI for the operations required by the manipulation of .DTA files
 if st.session_state["file_manager"] != None:
 
+    # Load a fresh set of variables from the session_state cache
     manager = st.session_state["file_manager"]
     suggested_ordering = manager.suggest_ordering()
 
-    with st.expander(
-        "Ordering selection", expanded=st.session_state["orderer_expander"]
-    ):
+    # Create a new expander for letting the user interact with the file ordering
+    with st.expander("Ordering selection", expanded=st.session_state["orderer_expander"]):
 
+        # Generate a table with number_input widgets to allow the user to select the proper file ordering
+        # Generate a header section
         with st.container():
             cname, ctstamp, ctype, ctime, corder = st.columns(5)
             with cname:
@@ -100,6 +105,7 @@ if st.session_state["file_manager"] != None:
             with corder:
                 st.markdown("**ID selector**")
 
+        # Generate the table body
         for level, block in enumerate(suggested_ordering):
             for n, entry in enumerate(block):
                 with st.container():
@@ -113,27 +119,32 @@ if st.session_state["file_manager"] != None:
                     with ctime:
                         st.write(manager.halfcycles[entry].time.iloc[-1])
                     with corder:
+                        # Save the user selection on a dictionary saved on the session_state cache
                         st.session_state["user_ordering_gui"][f"{level}:{n}"] = int(
                             st.number_input(
                                 "halfcycle ID:",
                                 min_value=0,
                                 value=level,
                                 step=1,
-                                key=level
-                                * st.session_state["number_of_halfcycles_files"]
-                                + n,
+                                key=level * st.session_state["number_of_halfcycles_files"]
+                                + n,  # Generate a custom key to univocally identify the widget
                             )
                         )
 
+        # Identify the maximum level (number of complete halfcycles) selected by the user
         max_level = max(st.session_state["user_ordering_gui"].values())
 
+        # Check for gaps in the level numbering
         missing = []
         for halfcycle_id in range(max_level):
             if halfcycle_id not in st.session_state["user_ordering_gui"].values():
                 missing.append(str(halfcycle_id))
 
+        # If a gap in the level numbering is detected print a warning, else show the build cycle button
         build = False
         if missing == []:
+
+            # Before builbing the cycle allow the user to activate/deactivate the built-in cleaning option
             st.session_state["clean_cycles"] = st.checkbox(
                 "Clean non-physical cycles (only efficiencies <100% and complete charge/discharge cycles)",
                 value=st.session_state["clean_cycles"],
@@ -146,46 +157,55 @@ if st.session_state["file_manager"] != None:
                 f"WARNING: The halfcycles IDs must be subsequent. The following IDs are missing: {missing_list}"
             )
 
+        # If the build cycle button is pressed, generate a custom ordering from the user input
         if build:
 
             custom_order = []
             for level in range(max_level):
                 custom_order.append([])
-                for key, selected_level in st.session_state[
-                    "user_ordering_gui"
-                ].items():
+                for key, selected_level in st.session_state["user_ordering_gui"].items():
                     if selected_level == level:
                         old_id = key.split(":")
                         custom_order[level].append(
                             suggested_ordering[int(old_id[0])][int(old_id[1])]
                         )
 
+            # Store the custom ordering in the session_state cache
             st.session_state["custom_ordering"] = custom_order
+
+            # Set the current expander as folded and force page rerun
             st.session_state["orderer_expander"] = False
             st.experimental_rerun()
 
+
 if st.session_state["custom_ordering"] != []:
 
+    # Reload a fresh set of variable from the cache
     manager = st.session_state["file_manager"]
+
+    # Generate the cycles list from the user-defined custom ordering
     st.session_state["cycles"] = manager.get_cycles(
         st.session_state["custom_ordering"], clean=st.session_state["clean_cycles"]
     )
 
+    # Look for hidden cycles
     st.session_state["hidden_cycles"] = 0
     for cycle in st.session_state["cycles"]:
         if cycle._hidden == True:
             st.session_state["hidden_cycles"] += 1
 
+    # Generate the cellcycling object from the user-defined custom ordering
     st.session_state["cellcycles"] = manager.build_cycles(
         st.session_state["custom_ordering"], clean=st.session_state["clean_cycles"]
     )
 
+    # Create a container to print a report about the loading operation just concluded
     with st.container():
         st.markdown("**General data:**")
         st.markdown("Instrument: ***{}***".format(manager.instrument))
 
+        # Print report on the number of loaded/parsed/skipped files, and on the joined/hidden cycles
         c1, c2, c3, c4, c5 = st.columns(5)
-
         with c1:
             st.metric("Uploaded files", value=st.session_state["num_uploaded_files"])
 
@@ -194,20 +214,19 @@ if st.session_state["custom_ordering"] != []:
 
         with c3:
             st.metric(
-                "Skipped files",
-                value=len(manager.bytestreams) - len(manager.halfcycles),
+                "Skipped files", value=len(manager.bytestreams) - len(manager.halfcycles),
             )
 
         with c4:
             st.metric(
                 "Halfcycles joined",
-                value=len(manager.halfcycles)
-                - len(st.session_state["custom_ordering"]),
+                value=len(manager.halfcycles) - len(st.session_state["custom_ordering"]),
             )
 
         with c5:
             st.metric("Hidden cycles", value=st.session_state["hidden_cycles"])
 
+        # Generate a non editable table with the final composition of each halfcycle
         table = []
         for level, level_list in enumerate(st.session_state["custom_ordering"]):
             for number, filename in enumerate(level_list):
@@ -228,10 +247,12 @@ if st.session_state["custom_ordering"] != []:
             columns=["Halfcycle", "Partial halfcycle", "Type", "Timestamp", "Filename"],
         )
 
+        # Print the halfcycle table in a dedicated expander
         with st.expander("Halfcycles file ordering report:", expanded=True):
             st.markdown("**Ordering report:**")
             st.table(df)
 
+        # Generate a non-editable table reporting the composition of each cycle
         table = []
         for cycle in st.session_state["cycles"]:
             charge_timestamp = (
@@ -250,14 +271,15 @@ if st.session_state["custom_ordering"] != []:
             table, columns=["Charge timestamp", "Discharge timestamp", "Hidden"]
         )
 
+        # Print the cycle table in a dedicated expander
         with st.expander("Cycles report after parsing:", expanded=True):
             st.markdown("**Cycles report:**")
             st.table(df)
 
-        st.success(
-            "SUCCESS: The dataset has been loaded and is now available for analysis"
-        )
+        # Print a success message to cheer up the user after the loading process
+        st.success("SUCCESS: The dataset has been loaded and is now available for analysis")
 
+# Always show a clear button that completly resets the application status
 clear_all = st.button("CLEAR ALL DATA")
 if clear_all:
     set_default()
--- cell-cycling/pages/cycle_inspector.py
+++ cell-cycling/pages/cycle_inspector.py
@@ -1,11 +1,32 @@
+import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from typing import Tuple
 from echemsuite.cellcycling.read_input import Cycle, HalfCycle
 
 
-def halfcycle_property_from_key(halfcycle: HalfCycle, key: str):
+def halfcycle_property_from_key(halfcycle: HalfCycle, key: str) -> Tuple[pd.Series, str]:
+    """
+    Extract from the streamlit session_state cache the cycle property and label
+    to be used in the plot.
+
+    Parameters
+    ----------
+    halfcycle : HalfCycle
+        halfcycle to be plotted.
+    key : str
+        keyword of the streamlit session_state cache dictionary encoding the
+        axis.
+
+    Returns
+    -------
+    series : pd.Series
+        data series containing the data to be plotted.
+    label : str
+        name of the data series.
+    """
     series, label = None, None
     if st.session_state[key] == "Time":
         series = halfcycle.time
@@ -23,39 +44,81 @@ def halfcycle_property_from_key(halfcycle: HalfCycle, key: str):
 
 
 def plot_halfcycle_mpl(halfcycle: HalfCycle, dpi: int = 600):
+    """
+    Generate a matplotlib plot of a given halfcycle automatically searching 
+    the streamlit session_state cache for the user-selected settings
 
+    Parameters
+    ----------
+    halfcycle : HalfCycle
+        halfcycle to be plotted.
+    dpi : int, optional
+        resolution of the figure object
+
+    Returns
+    -------
+    fig : Figure
+        matplotlib.pyplot figure object.
+
+    """
+    # Define the figure object and a primary set of axis (axa)
     fig, axa = plt.subplots(figsize=(10, 5), dpi=dpi)
 
+    # Extract series and label for the x and y_a axis (axa)
     x_axis, x_label = halfcycle_property_from_key(halfcycle, "inspector_x_scale")
     ya_axis, ya_label = halfcycle_property_from_key(halfcycle, "inspector_ya_scale")
 
+    # Generate the plot and set the labels
     axa.plot(x_axis, ya_axis, c="red", label=ya_label)
     axa.set_xlabel(x_label)
     axa.set_ylabel(ya_label, c="red")
+
+    # Set the plot grids
     axa.grid(which="major", c="#DDDDDD")
     axa.grid(which="minor", c="#EEEEEE")
 
+    # Check if a secondary value is selected for plot
     if st.session_state["inspector_yb_scale"] != "None":
+        axb = axa.twinx()  # Generate y_b as the x-twin of y_a
+
+        # Extract series and label for the y_b axis (axb)
         yb_axis, yb_label = halfcycle_property_from_key(halfcycle, "inspector_yb_scale")
 
-        axb = axa.twinx()
+        # Plot the data on the secondary axis
         axb.plot(x_axis, yb_axis, c="green", label=yb_label)
         axb.set_ylabel(yb_label, c="green")
 
     plt.tight_layout()
-
     return fig
 
 
-def plot_halfcycle_plotly(halfcycle: HalfCycle, dpi: int = 600):
+def plot_halfcycle_plotly(halfcycle: HalfCycle):
+    """
+    Generate a plotly plot of a given halfcycle automatically searching 
+    the streamlit session_state cache for the user-selected settings
 
+    Parameters
+    ----------
+    halfcycle : HalfCycle
+        halfcycle to be plotted.
+
+    Returns
+    -------
+    fig : Figure
+        plotly figure object.
+    """
+
+    # Create a figure object with the secondary y-axis option enabled
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
+    # Extract series and label for the x and y_a axis
     x_axis, x_label = halfcycle_property_from_key(halfcycle, "inspector_x_scale")
     ya_axis, ya_label = halfcycle_property_from_key(halfcycle, "inspector_ya_scale")
 
+    # Plot the y_a axis data on the primary axis
     fig.add_trace(go.Scatter(x=x_axis, y=ya_axis, name=ya_label), secondary_y=False)
 
+    # Apply proper formatting to the x and y_a axis
     fig.update_xaxes(
         title_text=x_label,
         showline=True,
@@ -72,11 +135,16 @@ def plot_halfcycle_plotly(halfcycle: HalfCycle, dpi: int = 600):
         gridcolor="#DDDDDD",
     )
 
+    # Check if a secondary value is selected for plot
     if st.session_state["inspector_yb_scale"] != "None":
 
+        # Extract series and label for the y_b axis (axb)
         yb_axis, yb_label = halfcycle_property_from_key(halfcycle, "inspector_yb_scale")
 
+        # Plot the y_b axis data on the secondary axis
         fig.add_trace(go.Scatter(x=x_axis, y=yb_axis, name=yb_label), secondary_y=True)
+
+        # Apply proper formatting to the y_b axis
         fig.update_yaxes(
             title_text=yb_label,
             secondary_y=True,
@@ -86,6 +154,7 @@ def plot_halfcycle_plotly(halfcycle: HalfCycle, dpi: int = 600):
             gridcolor=None,
         )
 
+    # Apply proper formatting to legend and plot background
     fig.update_layout(
         legend=dict(orientation="h", yanchor="bottom", y=1.0, xanchor="center", x=0.5),
         plot_bgcolor="#FFFFFF",
@@ -94,18 +163,29 @@ def plot_halfcycle_plotly(halfcycle: HalfCycle, dpi: int = 600):
     return fig
 
 
+### START OF THE APP PAGE BODY
+
+# Define local variables for the session_state cache (only those non affecting the global workflow)
 if "inspector_x_scale" not in st.session_state:
     st.session_state["inspector_x_scale"] = "Time"
     st.session_state["inspector_ya_scale"] = "Voltage"
     st.session_state["inspector_yb_scale"] = "Current"
 
+st.set_page_config(layout="wide")
+
 st.title("Cycle inspector")
 
+# Check if the cellcycles entry has been set else plot an info message
 if st.session_state["cellcycles"] != None:
+
+    # Prompt the user to select a cycle to inspect
     cycle: Cycle = st.selectbox("Select the cycle to inspect", st.session_state["cycles"])
+
+    # Check if the selected cycle is hidden ad inform the user with a warning box
     if cycle._hidden:
         st.warning("WARNING: The selected cycle is marked as hidden")
 
+    # Allow the user to select two parameters to plot on two independent axis (y_a and y_b)
     cx, cya, cyb = st.columns(3)
     with cx:
         x_axis_options = ["Time", "Charge", "Voltage", "Current"]
@@ -131,7 +211,9 @@ if st.session_state["cellcycles"] != None:
             index=yb_axis_options.index(st.session_state["inspector_yb_scale"]),
         )
 
+    # Check if the charge halfcycle is available
     if cycle.charge != None:
+        # Create an expander for the charge halfcycle plot
         with st.expander("Charge cycle data", expanded=True):
 
             st.markdown("### Charge")
@@ -141,7 +223,9 @@ if st.session_state["cellcycles"] != None:
             fig = plot_halfcycle_plotly(cycle.charge)
             st.plotly_chart(fig, use_container_width=True)
 
+    # Check if the discharge halfcycle is available
     if cycle.discharge != None:
+        # Create an expander for the discharge halfcycle plot
         with st.expander("Discharge cycle data", expanded=True):
 
             st.markdown("## Discharge")
@@ -151,8 +235,8 @@ if st.session_state["cellcycles"] != None:
             fig = plot_halfcycle_plotly(cycle.discharge)
             st.plotly_chart(fig, use_container_width=True)
 
-
 else:
+    # Print the info box redirecting the user to the upload page
     st.info(
         "HINT: In order to use this page you must first complete the upload and parsing of the files"
     )
